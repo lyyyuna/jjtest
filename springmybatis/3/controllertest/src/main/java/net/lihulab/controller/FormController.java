271c86c3ac2b8d3ac65eafee0bd649ad2d34f4d3
lyyyuna <lyyyuna@gmail.com>
2018-05-24 22:09:20 +0800

    modelattribute

---
+++ springmybatis/3/controllertest/src/main/java/net/lihulab/controller/FormController.java
@@ -0,0 +1,15 @@
+package net.lihulab.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FormController {
+
+    @RequestMapping(value="/{formname}")
+    public String loginForm(@PathVariable String formname) {
+        return formname;
+    }
+
+}

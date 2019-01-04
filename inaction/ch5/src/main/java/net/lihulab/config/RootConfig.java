2930ffb72d73643cabf5a5b04c5a020823b6838d
lyyyuna <lyyyuna@gmail.com>
2019-01-04 09:53:08 +0800

    update

--- inaction/ch5/src/main/java/net/lihulab/config/RootConfig.java
+++ inaction/ch5/src/main/java/net/lihulab/config/RootConfig.java
@@ -3,11 +3,13 @@ package net.lihulab.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.type.filter.RegexPatternTypeFilter;
 
 import java.util.regex.Pattern;
 
 @Configuration
+@Import(DataConfig.class)
 @ComponentScan(basePackages={"spittr"},
         excludeFilters={
                 @ComponentScan.Filter(type= FilterType.CUSTOM, value=RootConfig.WebPackage.class)

1508af523643e5bdb1947f015d5cb6292b708c00
lyyyuna <lyyyuna@gmail.com>
2019-01-04 10:49:43 +0800

    update

---
+++ springbootaction/spring-boot-01-hello-quick/src/main/java/net/lihulab/HelloWorldMainApplication.java
@@ -0,0 +1,11 @@
+package net.lihulab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class, args);
+    }
+}

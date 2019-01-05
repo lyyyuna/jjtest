71b04a548a164753a4ee6f9aafb3ba3b89f3b124
lyyyuna <lyyyuna@gmail.com>
2019-01-05 20:41:20 +0800

    update

--- springbootaction/spring-boot-03-logging/src/test/java/net/lihulab/DemoApplicationTests.java
+++ springbootaction/spring-boot-03-logging/src/test/java/net/lihulab/DemoApplicationTests.java
@@ -1,23 +1,21 @@
 package net.lihulab;
 
-import net.lihulab.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBoot02ConfigApplicationTests {
-
-    @Autowired
-	Person person;
+public class DemoApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	    System.out.println(person);
+		Logger logger = LoggerFactory.getLogger(getClass());
+		logger.warn("hello");
+
 	}
 
 }

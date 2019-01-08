7029bcd79395bcc2f08e9e44c8fe98adb99c75fd
lyyyuna <lyyyuna@gmail.com>
2019-01-08 22:24:32 +0800

    update

---
+++ springbootaction/spring-boot-06-jpa/src/main/java/net/lihulab/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package net.lihulab.repository;
+
+import net.lihulab.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}

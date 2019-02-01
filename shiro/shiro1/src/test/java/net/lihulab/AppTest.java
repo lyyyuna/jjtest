f77e06208c1fe66c52b50bc813c77a1b8f011583
lyyyuna <lyyyuna@gmail.com>
2019-02-01 14:35:58 +0800

    update

--- shiro/shiro1/src/test/java/net/lihulab/AppTest.java
+++ shiro/shiro1/src/test/java/net/lihulab/AppTest.java
@@ -2,6 +2,13 @@ package net.lihulab;
 
 import static org.junit.Assert.assertTrue;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
 import org.junit.Test;
 
 /**
@@ -15,6 +22,22 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+        // 1. 获取 SecurityManager 工厂，此处使用 ini 配置文件初始化 SecurityManager
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        // 2。得到 SecurityManager 实例，并绑定 SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        // 3. 得到 subject 及创建用户名/密码身份验证 token (即用户名/密码)
+        Subject subject = SecurityUtils.getSubject();
+        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "1123");
+        try {
+
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            System.out.println("wrong authentication: " + e.getMessage());
+        }
+
+        System.out.println(subject.isAuthenticated());
+
     }
 }
